91de81c2e3eec3b604944ead614caf00dec52dbe
deokbin <deokbin@naver.com>
2025-04-28 14:48:32 +0900

    04.28 알고리즘 풀이

---
+++ April/04.28/[1047]submittedSolution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function (s) {
+  const strArray = s.split("");
+  const stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+
+  return stack.join("");
+};
